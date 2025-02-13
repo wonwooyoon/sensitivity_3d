4408c5799c96824c7efa8a25d5e6c0a6cb6a9b33
wwy <yysam15@gmail.com>
2025-02-13 23:29:09 +0900

    bug fix

--- src/RunPFLOTRAN/RunPFLOTRAN.py
+++ src/RunPFLOTRAN/RunPFLOTRAN.py
@@ -4,23 +4,23 @@ import subprocess
 
 def run_pflotran_main():
     bash_code = """
-    #!/bin/bash
-    shopt -s extglob
-    base_dir="$(pwd)"
-    mkdir -p "${base_dir}/src/RunPFLOTRAN/output"
+        #!/bin/bash
+        shopt -s extglob
+        base_dir="$(pwd)"
+        mkdir -p "${base_dir}/src/RunPFLOTRAN/output"
 
-    for i in {1..50}; do
-        infile="${base_dir}/src/RunPFLOTRAN/input/sample_${i}.in"
-        mpirun -n 40 $PFLOTRAN_DIR/src/pflotran/pflotran -input_prefix "${infile%.*}"
-        output_subdir="${base_dir}/src/RunPFLOTRAN/output/$(basename ${infile%.*})"
-        mkdir -p "${output_subdir}"
-        mv ${base_dir}/src/RunPFLOTRAN/input/!(*ML_sensitivity_analysis).h5 "${output_subdir}"
-        mv ${base_dir}/src/RunPFLOTRAN/input/*.xmf "${output_subdir}"
-        mv ${base_dir}/src/RunPFLOTRAN/input/*.pft "${output_subdir}"
-        mv ${base_dir}/src/RunPFLOTRAN/input/*.out "${output_subdir}"
-        cp "${base_dir}/src/RunPFLOTRAN/input/sample_${i}.in" "${output_subdir}"
-    done
-    """
+        for i in {1..50}; do
+            infile="${base_dir}/src/RunPFLOTRAN/input/sample_${i}.in"
+            mpirun -n 40 $PFLOTRAN_DIR/src/pflotran/pflotran -input_prefix "${infile%.*}"
+            output_subdir="${base_dir}/src/RunPFLOTRAN/output/$(basename ${infile%.*})"
+            mkdir -p "${output_subdir}"
+            mv ${base_dir}/src/RunPFLOTRAN/input/!(mesh).h5 "${output_subdir}"
+            mv ${base_dir}/src/RunPFLOTRAN/input/*.xmf "${output_subdir}"
+            mv ${base_dir}/src/RunPFLOTRAN/input/*.pft "${output_subdir}"
+            mv ${base_dir}/src/RunPFLOTRAN/input/*.out "${output_subdir}"
+            cp "${base_dir}/src/RunPFLOTRAN/input/sample_${i}.in" "${output_subdir}"
+        done
+        """
     subprocess.run(['bash', '-c', bash_code], check=True)
 
 
